b663413bba427dba6fa60563cc5a0dfd4caff52c
Zoran Prstec <88144918+zoranprstec@users.noreply.github.com>
2022-04-08 12:51:17 +0200

    functionality implemented, missing styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import StartPage from "./StartPage"
+import QuestionPage from "./QuestionPage"
+import {useState, useEffect} from "react"
+
 export default function App () {
+    const [startPageActive, setStartPageActive] = useState(true)
+    const [quizData, setQuizData] = useState({
+        category: "",
+        difficulty: ""
+    })
+
     return (
-        <h1>Hello</h1>
+        <div>
+            {startPageActive && <StartPage setStartPage={setStartPageActive} quizData={quizData} setQuizData={setQuizData} />}
+            {!startPageActive && <QuestionPage setStartPage={setStartPageActive} quizData={quizData} />}
+        </div>
     )
 }
---
+++ src/QuestionCard.js
@@ -0,0 +1,78 @@
+import he from "he"                         // library za dekodiranje &quot; i sličnih gluposti
+import {useState, useEffect} from "react"
+
+export default function QuestionCard(props) {
+    const [answerData, setAnswerData] = useState({
+        [props.question]: ""                            // question: answer (malo čudno, znam; to je ono selectano)
+    })
+    const [answersArray, setAnswersArray] = useState(() => initializeArray())
+    const {[props.question]: answer} = answerData
+    
+    function initializeArray() {
+        const answersArray = props.incorrect_answers
+        answersArray.push(props.correct_answer)
+        return answersArray.sort()
+    }
+
+    function handleChange(event) {
+        const {value, name} = event.target
+        setAnswerData(prevAnswers => {
+            return {
+                ...prevAnswers,
+                [name]: value
+            }
+        })
+    }
+
+    let isCorrect = false
+
+    useEffect(() => {
+        isCorrect && props.setPoints(prevState => prevState + 1)
+    }, [props.submitted])
+    
+    const showAnswers = answersArray.map (
+        element => {
+            const selectedClass = answer === element ? "form-control-selected" : "form-control"
+            const correctClass = "form-control-correct"
+            const incorrectClass = "form-control-incorrect"
+            let submittedClass = ""
+
+            if (props.correct_answer === element) {
+                submittedClass = correctClass
+                if(answer === element) {
+                    isCorrect = true
+                }
+            } else if (answer === element && props.correct_answer !== element) {
+                submittedClass = incorrectClass
+            } else {
+                submittedClass = "form-control"
+            }
+
+            return (
+                <label key={`label${element}`} className={props.submitted ? submittedClass : selectedClass}>
+                    <input
+                        disabled={props.submitted}
+                        key={element}
+                        type="radio"
+                        id={element}
+                        name={props.question}
+                        value={element}
+                        checked={answer === element}
+                        onChange={handleChange}
+                    />
+                    {he.decode(element)}
+                </label>
+            )}
+    )
+    const decodedQuestion = he.decode(props.question)
+
+    return (
+        <section>
+            <h2>{decodedQuestion}</h2>
+            <div className="answers-row">
+                {showAnswers}
+            </div>
+            <hr />
+        </section>
+    )
+}
